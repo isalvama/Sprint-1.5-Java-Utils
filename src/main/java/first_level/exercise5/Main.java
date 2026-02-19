edcf36ba7bea81ac6995c6d059ec4958fc2de6f9
isalvama <inesalvans@gmail.com>
2026-02-19 12:51:40 +0100

    fix: adapt code to make it work through commands refactor: move all the core functionalities to User class and implement them in 'serializeUser()' and 'deserializeUser()' methods

--- src/main/java/first_level/exercise5/Main.java
+++ src/main/java/first_level/exercise5/Main.java
@@ -1,30 +1,18 @@
 package first_level.exercise5;
 
 import java.io.*;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args){
-        User user1 = new User("1234A", "isalvama");
-        try{FileOutputStream fileOutputStream
-                = new FileOutputStream("/Users/ines/sfos.ser");
-      ObjectOutputStream objectOutputStream
-                = new ObjectOutputStream(fileOutputStream);
-        objectOutputStream.writeObject(user1);
-        objectOutputStream.flush();
-        objectOutputStream.close();
 
-        FileInputStream fileInputStream
-                = new FileInputStream("/Users/ines/sfos.ser");
-        ObjectInputStream objectInputStream
-                = new ObjectInputStream(fileInputStream);
-        User user2 = (User) objectInputStream.readObject();
-            System.out.println(user2);
-
-            objectInputStream.close(); } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        if (args.length == 0) {
+            System.out.println("There are no arguments.\n Type: java first_level.exercise5.Main <path of serialized file to create>\"");
         }
+        String outputPath = args[0];
+        User user1 = new User("1234A", "isalvama");
+            user1.serializeUser(outputPath);
+            user1.deserializeUser(outputPath);
     }
-    }
+}
 
--- src/main/java/first_level/exercise5/User.java
+++ src/main/java/first_level/exercise5/User.java
@@ -1,18 +1,40 @@
 package first_level.exercise5;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.Objects;
 
 public class User implements Serializable {
     private final String id;
     private String userName;
     private boolean isPremium;
     private int points;
+
     public User( String id,
     String userName){
         this.id = id;
        this.userName = userName;
        isPremium = false;
         points = 0;}
+
+    public void serializeUser(String outputPath) {
+        String fileOutputPath = outputPath + "user.ser";
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(fileOutputPath))){
+            objectOutputStream.writeObject(this);
+            objectOutputStream.flush();
+        } catch (IOException ioe){
+            System.err.println("Error: " + ioe.getMessage());
+        }
+    }
+
+    public void deserializeUser (String inputPath){
+        String fileInputPath = inputPath + "user.ser";
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(fileInputPath))){
+            User user2 = (User) objectInputStream.readObject();
+            System.out.println(user2);
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
 
     @Override
     public String toString() {
