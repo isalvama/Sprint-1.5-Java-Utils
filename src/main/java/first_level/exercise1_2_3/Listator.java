d73edd8297ccdb7b45e904ed89b5c6d68104d80a
isalvama <inesalvans@gmail.com>
2026-02-19 12:47:40 +0100

    fix: remove unnecessary spaces in code feat: refactor writePathsInTxtFile() and readPathsInTxtFile methods to work with main changes in order to be executed from terminal

--- src/main/java/first_level/exercise1_2_3/Listator.java
+++ src/main/java/first_level/exercise1_2_3/Listator.java
@@ -1,8 +1,6 @@
 package first_level.exercise1_2_3;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.attribute.FileTime;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -11,7 +9,6 @@ import java.util.*;
 public class Listator {
 
     public static String getPathContentSorted(String path, int level) {
-
         File directory = new File(path);
         if (!(directory.exists())) {
             return "The entered directory does not exist";
@@ -24,7 +21,6 @@ public class Listator {
         Collections.sort(files);
         String content = "";
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - hh:mm:ss");
-
         for (File file : files) {
             content += file.toString();
             String fileTime = dateFormat.format(file.lastModified());
@@ -39,20 +35,20 @@ public class Listator {
         return content;
     }
 
-    public static void writePathsInTxtFile(String pathToRead, String filePathToCreate) throws IOException {
-        try (
-                BufferedWriter bw = new BufferedWriter(new FileWriter(filePathToCreate));)
-        {bw.write(getPathContentSorted(pathToRead, 0));
+    public static void writePathsInTxtFile(String inputDirectoryPath, String outputFilePath) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilePath))){
+            bw.write(getPathContentSorted(inputDirectoryPath, 0));
         } catch(IOException ioe){
             System.out.println(ioe.getMessage());
         }
     }
 
     public static void readPathsInTxtFile(String filePathToRead) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(filePathToRead));){
+        try (BufferedReader br = new BufferedReader(new FileReader(filePathToRead))){
             String line;
             while((line = br.readLine()) != null){
-                System.out.println(line);}
+                System.out.println(line);
+            }
         } catch(IOException ioe){
             System.out.println(ioe.getMessage());
         }
